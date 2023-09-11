6a89ab6f6396893c6d23c27ac81bdcbb127321bb
David Figueroa <davidfigueroa0327@gmail.com>
2023-09-11 18:29:08 -0500

    feat(react): :sparkles: is mobile hook added
    A hook to get a boolean if the current viewport is mobile size was added

---
+++ src/hooks/use-is-mobile.ts
@@ -0,0 +1,17 @@
+import { useLayoutEffect, useState } from "react";
+import debounce from "lodash/debounce";
+
+const useIsMobile = (): boolean => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useLayoutEffect(() => {
+    const updateSize = () => setIsMobile(window.innerWidth < 768);
+    window.addEventListener("resize", debounce(updateSize, 250));
+
+    return (): void => window.removeEventListener("resize", updateSize);
+  }, []);
+
+  return isMobile;
+};
+
+export default useIsMobile;
