3a458f02fe3c1a96eef39c729c1318cc23dcfb3b
David Figueroa <davidfigueroa0327@gmail.com>
2023-09-12 06:37:34 -0500

    refactor(api): :recycle: move todo router to own file

--- functions/api/[[route]].ts
+++ functions/api/[[route]].ts
@@ -1,13 +1,9 @@
 import { Hono } from "hono";
 import { handle } from "hono/cloudflare-pages";
-import { z } from "zod";
-import { zValidator } from "@hono/zod-validator";
-import { todos as todosTable } from "../../db/schema";
-import { drizzle } from "drizzle-orm/d1";
 import { logger } from "hono/logger";
-import { eq } from "drizzle-orm";
+import todoRouter from "./todo";
 
-type Bindings = {
+export type Bindings = {
   SHARED_STORAGE_DB: D1Database;
 };
 
@@ -15,68 +11,7 @@ const app = new Hono<{ Bindings: Bindings }>();
 
 app.use("*", logger());
 
-const route = app
-  .basePath("/api")
-  .post(
-    "/todo",
-    zValidator("form", z.object({ message: z.string() })),
-    async (c) => {
-      const todo = c.req.valid("form");
-      const db = drizzle(c.env.SHARED_STORAGE_DB);
-
-      await db.insert(todosTable).values({ message: todo.message });
-      return c.jsonT({ message: "created!" });
-    }
-  )
-  .get(async (c) => {
-    const db = drizzle(c.env.SHARED_STORAGE_DB);
-    const todos = await db.select().from(todosTable).all();
-
-    return c.jsonT({ todos });
-  })
-  .patch(
-    "/todo/:id",
-    zValidator(
-      "form",
-      z.object({
-        status: z.string().pipe(z.coerce.number()),
-        message: z.string(),
-      })
-    ),
-    zValidator(
-      "param",
-      z.object({
-        id: z.string().pipe(z.coerce.number()),
-      })
-    ),
-    async (c) => {
-      const form = c.req.valid("form");
-      const id = c.req.valid("param").id;
-      const db = drizzle(c.env.SHARED_STORAGE_DB);
-
-      const updated = await db
-        .update(todosTable)
-        .set({ status: form.status === 1, message: form.message })
-        .where(eq(todosTable.id, id))
-        .returning();
-
-      return c.jsonT(updated);
-    }
-  )
-  .delete(
-    "/todo/:id",
-    zValidator("param", z.object({ id: z.string().pipe(z.coerce.number()) })),
-    async (c) => {
-      const id = c.req.valid("param").id;
-      const db = drizzle(c.env.SHARED_STORAGE_DB);
-
-      await db.delete(todosTable).where(eq(todosTable.id, id));
-
-      c.status(201);
-
-      return c.text("");
-    }
-  );
+const route = app.basePath("/api").route("/todo", todoRouter);
 
 export type AppType = typeof route;
 
---
+++ functions/api/todo.ts
@@ -0,0 +1,72 @@
+import { zValidator } from "@hono/zod-validator";
+import { eq } from "drizzle-orm";
+import { drizzle } from "drizzle-orm/d1";
+import { Hono } from "hono";
+import { z } from "zod";
+import { todos as todosTable } from "../../db/schema";
+import { Bindings } from "./[[route]]";
+
+const route = new Hono<{ Bindings: Bindings }>();
+
+route
+  .post(
+    "/",
+    zValidator("form", z.object({ message: z.string() })),
+    async (c) => {
+      const todo = c.req.valid("form");
+      const db = drizzle(c.env.SHARED_STORAGE_DB);
+
+      await db.insert(todosTable).values({ message: todo.message });
+      return c.jsonT({ message: "created!" });
+    }
+  )
+  .get(async (c) => {
+    const db = drizzle(c.env.SHARED_STORAGE_DB);
+    const todos = await db.select().from(todosTable).all();
+
+    return c.jsonT({ todos });
+  })
+  .patch(
+    "/:id",
+    zValidator(
+      "form",
+      z.object({
+        status: z.string().pipe(z.coerce.number()),
+        message: z.string(),
+      })
+    ),
+    zValidator(
+      "param",
+      z.object({
+        id: z.string().pipe(z.coerce.number()),
+      })
+    ),
+    async (c) => {
+      const form = c.req.valid("form");
+      const id = c.req.valid("param").id;
+      const db = drizzle(c.env.SHARED_STORAGE_DB);
+
+      const updated = await db
+        .update(todosTable)
+        .set({ status: form.status === 1, message: form.message })
+        .where(eq(todosTable.id, id))
+        .returning();
+
+      return c.jsonT(updated);
+    }
+  )
+  .delete(
+    zValidator("param", z.object({ id: z.string().pipe(z.coerce.number()) })),
+    async (c) => {
+      const id = c.req.valid("param").id;
+      const db = drizzle(c.env.SHARED_STORAGE_DB);
+
+      await db.delete(todosTable).where(eq(todosTable.id, id));
+
+      c.status(201);
+
+      return c.text("");
+    }
+  );
+
+export default route;
